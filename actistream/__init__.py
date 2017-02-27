fcd5807b7973e2b1704d7a8fd8de216ecf264916
Raymond Penners <raymond.penners@intenct.nl>
2017-02-27 18:01:35 +0100

    Released 1.0.5

--- actistream/__init__.py
+++ actistream/__init__.py
@@ -1,6 +1,6 @@
 from importlib import import_module
 
-VERSION = (1, 0, 4, 'final', 0)
+VERSION = (1, 0, 5, 'final', 0)
 
 __title__ = 'django-actistream'
 __version_info__ = VERSION
@@ -8,7 +8,7 @@ __version__ = '.'.join(map(str, VERSION[:3])) + ('-{}{}'.format(
     VERSION[3], VERSION[4] or '') if VERSION[3] != 'final' else '')
 __author__ = 'Raymond Penners'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2016 Raymond Penners and contributors'
+__copyright__ = 'Copyright 2017 Raymond Penners and contributors'
 
 
 class ActivityTypeRegistry(object):
